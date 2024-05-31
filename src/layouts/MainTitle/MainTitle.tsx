4efa4d35554a54cbac4094e590cd3d84c105e334
StrongerProgrammer7 <swat55551@gmail.com>
2024-05-31 17:57:18 +0300

    Added layout main titel
    Decomposition Main - created layout MainTitle

---
+++ src/layouts/MainTitle/MainTitle.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import css from "./maintitle.module.css";
+
+interface IMainTitle
+{
+  url_img: string;
+  title: string;
+  text: string;
+
+}
+const MainTitle: FC<IMainTitle> = ({ url_img,title,text }) =>
+{
+  return (
+    <>
+      <div className={css.wrapper_img}>
+        <img src={url_img} alt="img greeter" />
+      </div>
+      <div className={css.content}>
+        <h1 className={css.content__title}>{title}</h1>
+        <p className={css.content__text}>{text}</p>
+
+      </div>
+    </>
+  );
+};
+
+export default MainTitle;
