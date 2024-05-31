491442fe282f9efd6b91d916d2ca9fa8caf18d6f
StrongerProgrammer7 <swat55551@gmail.com>
2024-05-31 12:43:03 +0300

    Added main content (first page)

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,18 @@
-
+import css from "./main.module.css";
+import main from "../../assets/main.png";
 
 const Main = () =>
 {
 	return (
-		<div>Main</div>
+		<main className={css.main_content}>
+			<div className={css.main_content__img}>
+				<img src={main} alt="hello img" />
+			</div>
+			<div className={css.main_content__wrapper}>
+				<h1 className={css.main_content__title}>Уважаемый клиент!</h1>
+				<p className={css.main_content__text}>Запрос закрыт. Пожалуйста, оцените качество предоставленного сервиса по данному обращению, используя шкалу от 0 до 9, где 0 является «Хуже некуда» и 9 — «Отлично».</p>
+			</div>
+		</main>
 	);
 };
 
